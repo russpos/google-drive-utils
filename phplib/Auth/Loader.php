a3fb4593a28710877ce671d96179dd88c372254a
Russ Pos <Russ Posluszny>
2017-02-13 21:50:47 -0500

    Update samples

--- phplib/Auth/Loader.php
+++ phplib/Auth/Loader.php
@@ -2,7 +2,6 @@
 
 class Loader {
 
-    private $auth_token;
     private $client_data;
     private $scope;
 
@@ -12,13 +11,15 @@ class Loader {
     private $token_store = null;
     private $loader;
 
-    const AUTH_URL_BASE = "https://accounts.google.com/o/oauth2/auth?";
-    const CLI_REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob";
+    const RESPONSE_TYPE_TOKEN = "token";
+    const RESPONSE_TYPE_CODE  = "code";
 
     /**
      * __construct
      * Creates a new Auth/Loader
      * @param ClientData $client_data ClientData object of the application you are loading authorization for
+     * @param TokenStorageInterface $token_store Cache where this token should be stored.
+     *    Check here first, and also store it here if its not present.
      * @param Scope $scope The scope you are attempting to load authorization for
      * @access public
      */
@@ -46,18 +47,16 @@ class Loader {
      */
     private function getAuthURL() : string {
         $payload = [
-            "client_id"     => $this->client_data->getClientId(),
-            "scope"         => $this->scope->toString(),
+            "client_id" => $this->client_data->getClientId(),
+            "scope"     => $this->scope->toString(),
         ];
 
         if ($this->with_redirect) {
             $redirect = $this->client_data->getRedirectUri(1);
-            // TODO: Make const
-            $payload['response_type'] = "token";
+            $payload['response_type'] = self::RESPONSE_TYPE_TOKEN;
         } else {
             $redirect = $this->client_data->getRedirectUri(0);
-            // TODO: Make const
-            $payload['response_type'] = "code";
+            $payload['response_type'] = self::RESPONSE_TYPE_CODE;
         }
 
         $payload["redirect_uri"] = (empty($this->redirect_params)) ?
@@ -65,6 +64,13 @@ class Loader {
         return $this->client_data->getAuthURI(). '?'. http_build_query($payload);
     }
 
+    /**
+     * getTokenWithInterface
+     * Returns a Token object, with user authorization happening through the provided interface
+     * @param UserInterface $ui The user interface through which the authorization takes place
+     * @access public
+     * @return Token Token data - either from the cache or fetched from the server
+     */
     public function getTokenWithInterface(UserInterface $ui) : Token {
         try {
             $token = $this->token_store->loadToken();
--- sample.php
+++ sample.php
@@ -2,15 +2,10 @@
 use Russpos\GoogleDriveUtils\Auth;
 require "loader.php";
 
-$client_data = Auth\ClientData::fromFile(__DIR__."/client_id.json");
-json_decode(file_get_contents(__DIR__."/client_id.json"));
-
-$client_data->print_r();
-
-$file_storage = new Auth\TokenFileStorage("/Users/russp/.gdrive");
+$client_data    = Auth\ClientData::fromFile(__DIR__."/client_id.json");
+$file_storage   = new Auth\TokenFileStorage("/Users/russp/.gdrive");
 $command_loader = new Auth\Loader($client_data, $file_storage, Auth\CalendarScope::manage());
 
 $cli = new Auth\Cli();
-
 $token = $command_loader->getTokenWithInterface($cli);
 echo "Token!\n"; print_r($token);
